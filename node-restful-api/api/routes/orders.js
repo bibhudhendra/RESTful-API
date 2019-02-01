92c8a67abcc3342ffc01015987295202e2727c7d
bibhudhendra <31999344+bibhudhendra@users.noreply.github.com>
2019-02-02 02:29:36 +0530

    Add files via upload

---
+++ node-restful-api/api/routes/orders.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const verifyauth = require('../middleware/verifyauth');
+
+const Order = require("../models/orderschema");
+const Product = require("../models/productschema");
+
+router.get("/", verifyauth, (req, res, next) => {
+  Order.find()
+
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map(doc => {
+          return {
+            _id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            
+          };
+        })
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.post("/", verifyauth, (req, res, next) => {
+  Product.findById(req.body.productId)
+    .then(product => {
+      if (!product) {
+        return res.status(404).json({
+          message: "Product not found"
+        });
+      }
+      const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+      });
+      return order.save();
+    })
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Order stored",
+        createdOrder: {
+          _id: result._id,
+          product: result.product,
+          quantity: result.quantity
+        },
+        
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.get("/:orderId", verifyauth, (req, res, next) => {
+  Order.findById(req.params.orderId)
+    .populate('product')
+    .exec()
+    .then(order => {
+      if (!order) {
+        return res.status(404).json({
+          message: "Order not found"
+        });
+      }
+      res.status(200).json({
+        order: order,
+        
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+router.delete("/:orderId", verifyauth, (req, res, next) => {
+  Order.remove({ _id: req.params.orderId })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "Order deleted"
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+module.exports = router;
