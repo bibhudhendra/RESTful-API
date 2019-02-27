492fa46fe51aa03b354a96d5cc76fae00f27b505
bibhudhendra <31999344+bibhudhendra@users.noreply.github.com>
2019-02-27 14:59:48 +0530

    Update products.js

--- node-restful-api/api/routes/products.js
+++ node-restful-api/api/routes/products.js
@@ -1,34 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const multer = require('multer');
 const verifyauth = require('../middleware/verifyauth');
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, './uploads/');
-  },
-  filename: function(req, file, cb) {
-    cb(null, new Date().toISOString() + file.originalname);
-  }
-});
-
-const fileFilter = (req, file, cb) => {
-  // reject a file
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  },
-  fileFilter: fileFilter
-});
 
 const Product = require("../models/productschema");
 
@@ -65,7 +39,7 @@ router.get("/", (req, res, next) => {
     });
 });
 
-router.post("/", verifyauth, upload.single('productImage'), (req, res, next) => {
+router.post("/", verifyauth, (req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
